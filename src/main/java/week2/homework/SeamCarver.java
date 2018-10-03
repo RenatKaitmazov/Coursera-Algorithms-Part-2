259286f90a0404ac31a5d31fa35862d940bf6b97
Renat Kaitmazov <renatkaitmazov@gmail.com>
2018-10-03 23:58:59 +0300

    Find vertical seam

--- src/main/java/week2/homework/SeamCarver.java
+++ src/main/java/week2/homework/SeamCarver.java
@@ -2,6 +2,8 @@ package week2.homework;
 
 import edu.princeton.cs.algs4.Picture;
 
+import java.util.Arrays;
+
 /**
  * See <a href="http://coursera.cs.princeton.edu/algs4/assignments/seam.html">description</a>
  *
@@ -10,8 +12,16 @@ import edu.princeton.cs.algs4.Picture;
 
 public final class SeamCarver {
 
+    /*--------------------------------------------------------*/
+    /* Static variables                                       */
+    /*--------------------------------------------------------*/
+
     private static final double DEFAULT_ENERGY = 1_000.0;
     private static final int[] EMPTY_ARRAY = new int[0];
+
+    /*--------------------------------------------------------*/
+    /* Fields                                                 */
+    /*--------------------------------------------------------*/
 
     private final Picture picture;
 
@@ -31,6 +41,10 @@ public final class SeamCarver {
     public Picture picture() {
         return picture;
     }
+
+    /*--------------------------------------------------------*/
+    /* API                                                    */
+    /*--------------------------------------------------------*/
 
     public int width() {
         return picture.width();
@@ -62,8 +76,97 @@ public final class SeamCarver {
     }
 
     public int[] findVerticalSeam() {
-        // TODO 2) Add implementation
-        return EMPTY_ARRAY;
+        final int cols = width();
+        final int rows = height();
+        final int[] path = new int[rows];
+        final double[] cost = new double[rows];
+        for (int i = 0; i < rows; ++i) {
+            cost[i] = Double.POSITIVE_INFINITY;
+        }
+        // Start finding the shortest path from the upper left corner
+        cost[0] = energy(0, 0);
+        findShortestVerticalPathFromOrigin(path, cost, rows, cols);
+        final int[] auxPath = new int[rows];
+        final double[] auxCost = new double[rows];
+        for (int col = 1; col < cols; ++col) {
+            findShortestVerticalPath(path, auxPath, cost, auxCost, col, rows, cols);
+        }
+        return path;
+    }
+
+    private void findShortestVerticalPathFromOrigin(int[] path,
+                                                    double[] cost,
+                                                    final int rows,
+                                                    final int cols) {
+        int shortestCol = 0;
+        for (int row = 1; row < rows; ++row) {
+            final int colBound = shortestCol + 2;
+            double minEnergy = Double.POSITIVE_INFINITY;
+            int minCol = shortestCol - 1;
+            for (int col = minCol; col < colBound; ++col) {
+                // Out of bounds
+                if (col < 0 || col >= cols) continue;
+                final double energy = energy(col, row);
+                // Find at which column the minimal energy is
+                if (energy < minEnergy) {
+                    minEnergy = energy;
+                    minCol = col;
+                    shortestCol = minCol;
+                }
+            }
+            path[row] = minCol;
+            cost[row] = cost[row - 1] + minEnergy;
+        }
+    }
+
+    private void findShortestVerticalPath(int[] oldPath,
+                                          int[] path,
+                                          double[] oldCost,
+                                          double[] cost,
+                                          int startCol,
+                                          final int rows,
+                                          final int cols) {
+        path[0] = startCol;
+        cost[0] = energy(startCol, 0);
+        int shortestCol = startCol;
+        for (int row = 1; row < rows; ++row) {
+            final int colBound = shortestCol + 2;
+            double minEnergy = Double.POSITIVE_INFINITY;
+            int minCol = shortestCol - 1;
+            for (int col = minCol; col < colBound; ++col) {
+                // Out of bounds
+                if (col < 0 || col >= cols) continue;
+                final double energy = energy(col, row);
+                // Find at which column the minimal energy is
+                if (energy < minEnergy) {
+                    minEnergy = energy;
+                    minCol = col;
+                    shortestCol = minCol;
+                }
+            }
+            if (oldPath[row] == minCol) {
+                // There is an intersection between the old shortest path and the new one,
+                // Just update everything up to the intersection point.
+                if (oldCost[row] > cost[row - 1] + minEnergy) {
+                    for (int i = 0; i < row; ++i) {
+                        oldPath[i] = path[i];
+                        oldCost[i] = cost[i];
+                    }
+                }
+                return;
+            } else {
+                path[row] = minCol;
+                cost[row] = cost[row - 1] + minEnergy;
+            }
+        }
+        // At this point we know we found a new unique path and we need to check if the new path
+        // is shorter than the old one.
+        if (oldCost[rows - 1] > cost[rows - 1]) {
+            for (int row = 0; row < rows; ++row) {
+                oldPath[row] = path[row];
+                oldCost[row] = cost[row];
+            }
+        }
     }
 
     public void removeHorizontalSeam(int[] seam) {
@@ -109,5 +212,19 @@ public final class SeamCarver {
 
     private int blue(int rgb) {
         return rgb & 0x000000FF;
+    }
+
+    public static void main(String[] args) {
+        final String path = "/Users/RenatKaitmazov/Desktop/seam/10x12.png";
+        final Picture picture = new Picture(path);
+        final SeamCarver carver = new SeamCarver(picture);
+        for (int row = 0; row < carver.height(); ++row) {
+            for (int col = 0; col < carver.width(); ++col) {
+                System.out.printf("%.2f\t", carver.energy(col, row));
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(carver.findVerticalSeam()));
     }
 }
