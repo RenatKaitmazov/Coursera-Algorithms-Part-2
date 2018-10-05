c73282c623ac1015d6595bd356300d17f76badd8
Renat Kaitmazov <renatkaitmazov@gmail.com>
2018-10-06 00:16:19 +0300

    Temporary refactoring for backup

--- src/main/java/week2/homework/SeamCarver.java
+++ src/main/java/week2/homework/SeamCarver.java
@@ -2,8 +2,6 @@ package week2.homework;
 
 import edu.princeton.cs.algs4.Picture;
 
-import java.util.Arrays;
-
 /**
  * See <a href="http://coursera.cs.princeton.edu/algs4/assignments/seam.html">description</a>
  *
@@ -16,14 +14,29 @@ public final class SeamCarver {
     /* Static variables                                       */
     /*--------------------------------------------------------*/
 
+    /**
+     * The width of borders in pixels.
+     */
+    private static final int BORDER_WIDTH = 1;
+    /**
+     * If the width or the height of an image is less than this number, it means
+     * the is no any pixel whose energy is not 1000.
+     */
+    private static final int MIN_SIZE = 3;
+    /**
+     * The default energy of pixel that is located right on the boarder of an image.
+     */
     private static final double DEFAULT_ENERGY = 1_000.0;
-    private static final int[] EMPTY_ARRAY = new int[0];
 
     /*--------------------------------------------------------*/
     /* Fields                                                 */
     /*--------------------------------------------------------*/
 
-    private final Picture picture;
+    /**
+     * Used when performing transformations.
+     */
+    private final Point point = new Point();
+    private Picture picture;
 
     /*--------------------------------------------------------*/
     /* Constructors                                           */
@@ -54,44 +67,54 @@ public final class SeamCarver {
         return picture.height();
     }
 
-    public double energy(int x, int y) {
-        checkPointInRange(x, y);
-        final int leftX = x - 1;
-        final int rightX = x + 1;
-        final int topY = y - 1;
-        final int bottomY = y + 1;
+    public double energy(int col, int row) {
+        checkPointInRange(col, row);
+        final int leftX = col - 1;
+        final int rightX = col + 1;
+        final int topY = row - 1;
+        final int bottomY = row + 1;
         if ((leftX < 0 || topY < 0) || (rightX == width() || bottomY == height())) return DEFAULT_ENERGY;
-        final int leftPixelRgb = picture.getRGB(leftX, y);
-        final int rightPixelRgb = picture.getRGB(rightX, y);
+        final int leftPixelRgb = picture.getRGB(leftX, row);
+        final int rightPixelRgb = picture.getRGB(rightX, row);
         final int horizontalDelta = delta(leftPixelRgb, rightPixelRgb);
-        final int topPixelRgb = picture.getRGB(x, topY);
-        final int bottomPixelRgb = picture.getRGB(x, bottomY);
+        final int topPixelRgb = picture.getRGB(col, topY);
+        final int bottomPixelRgb = picture.getRGB(col, bottomY);
         final int verticalDelta = delta(topPixelRgb, bottomPixelRgb);
         return Math.sqrt(horizontalDelta + verticalDelta);
     }
 
     public int[] findHorizontalSeam() {
-        // TODO 1) Add implementation
-        return EMPTY_ARRAY;
+        final int rows = height();
+        final int cols = width();
+        final int[] seam = new int[cols];
+        if (noPixelsBetweenBorders(rows, cols)) {
+            return seam;
+        }
+        return seam;
     }
 
     public int[] findVerticalSeam() {
-        final int cols = width();
         final int rows = height();
-        final int[] path = new int[rows];
-        final double[] cost = new double[rows];
-        for (int i = 0; i < rows; ++i) {
-            cost[i] = Double.POSITIVE_INFINITY;
+        final int cols = width();
+        final int[] seam = new int[rows];
+        if (noPixelsBetweenBorders(rows, cols)) {
+            return seam;
         }
-        // Start finding the shortest path from the upper left corner
-        cost[0] = energy(0, 0);
-        findShortestVerticalPathFromOrigin(path, cost, rows, cols);
-        final int[] auxPath = new int[rows];
-        final double[] auxCost = new double[rows];
-        for (int col = 1; col < cols; ++col) {
-            findShortestVerticalPath(path, auxPath, cost, auxCost, col, rows, cols);
+        // We have two vertical borders as well as two horizontal borders but the energy of pixels on the border
+        // is 1000 so we do not include them to be a little bit efficient.
+        final int borderCount = 2;
+        final int energyRows = rows - borderCount;
+        final int energyCols = cols - borderCount;
+        final int size = energyRows * energyCols;
+        final int[] route = new int[size];
+        final double[] energy = newEnergyArray(size, energyCols);
+        final int firstRow = 1;
+        for (int col = 0; col < energyCols; ++col) {
+            energy[col] = energy(col + BORDER_WIDTH, firstRow);
         }
-        return path;
+        seam[0] = seam[1] - 1;
+        seam[rows - 1] = seam[rows - 2] - 1;
+        return seam;
     }
 
     public void removeHorizontalSeam(int[] seam) {
@@ -116,7 +139,7 @@ public final class SeamCarver {
 
     private void checkPointInRange(int x, int y) {
         if ((x < 0 || x >= width()) || (y < 0 || y >= height())) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("x: " + x + "; y: " + y);
         }
     }
 
@@ -139,91 +162,34 @@ public final class SeamCarver {
         return rgb & 0x000000FF;
     }
 
-    private void findShortestVerticalPathFromOrigin(int[] path,
-                                                    double[] cost,
-                                                    final int rows,
-                                                    final int cols) {
-        int shortestCol = 0;
-        for (int row = 1; row < rows; ++row) {
-            final int colBound = shortestCol + 2;
-            double minEnergy = Double.POSITIVE_INFINITY;
-            int minCol = shortestCol - 1;
-            for (int col = minCol; col < colBound; ++col) {
-                // Out of bounds
-                if (col < 0 || col >= cols) continue;
-                final double energy = energy(col, row);
-                // Find at which column the minimal energy is
-                if (energy < minEnergy) {
-                    minEnergy = energy;
-                    minCol = col;
-                    shortestCol = minCol;
-                }
-            }
-            path[row] = minCol;
-            cost[row] = cost[row - 1] + minEnergy;
-        }
+    private boolean noPixelsBetweenBorders(int rows, int columns) {
+        return rows < MIN_SIZE || columns < MIN_SIZE;
     }
 
-    private void findShortestVerticalPath(int[] oldPath,
-                                          int[] path,
-                                          double[] oldCost,
-                                          double[] cost,
-                                          int startCol,
-                                          final int rows,
-                                          final int cols) {
-        path[0] = startCol;
-        cost[0] = energy(startCol, 0);
-        int shortestCol = startCol;
-        for (int row = 1; row < rows; ++row) {
-            final int colBound = shortestCol + 2;
-            double minEnergy = Double.POSITIVE_INFINITY;
-            int minCol = shortestCol - 1;
-            for (int col = minCol; col < colBound; ++col) {
-                // Out of bounds
-                if (col < 0 || col >= cols) continue;
-                final double energy = energy(col, row);
-                // Find at which column the minimal energy is
-                if (energy < minEnergy) {
-                    minEnergy = energy;
-                    minCol = col;
-                    shortestCol = minCol;
-                }
-            }
-            if (oldPath[row] == minCol) {
-                // There is an intersection between the old shortest path and the new one,
-                // Just update everything up to the intersection point.
-                if (oldCost[row] > cost[row - 1] + minEnergy) {
-                    for (int i = 0; i < row; ++i) {
-                        oldPath[i] = path[i];
-                        oldCost[i] = cost[i];
-                    }
-                    // Update cost.
-                    final double delta = oldCost[row] - (oldCost[row - 1] + minEnergy);
-                    for (int i = row; i < rows; ++i) {
-                        oldCost[i] -= delta;
-                    }
-                }
-                return;
-            } else {
-                path[row] = minCol;
-                cost[row] = cost[row - 1] + minEnergy;
-            }
+    private double[] newEnergyArray(int size, int cols) {
+        final double[] energy = new double[size];
+        for (int i = cols; i < size; ++i) {
+            energy[i] = Double.POSITIVE_INFINITY;
         }
-        // At this point we know we found a new unique path and we need to check if the new path
-        // is shorter than the old one.
-        if (oldCost[rows - 1] > cost[rows - 1]) {
-            for (int row = 0; row < rows; ++row) {
-                oldPath[row] = path[row];
-                oldCost[row] = cost[row];
-            }
-        }
+        return energy;
     }
 
-    public static void main(String[] args) {
-        final String path = "/Users/RenatKaitmazov/Desktop/seam/7x10.png";
-        final Picture picture = new Picture(path);
-        final SeamCarver carver = new SeamCarver(picture);
-        System.out.println();
-        System.out.println(Arrays.toString(carver.findVerticalSeam()));
+    private int pointToIndex(int row, int col, int cols) {
+        return row * cols + col;
+    }
+
+    private Point indexToPoint(int index, int cols) {
+        point.row = index / cols;
+        point.col = index % cols;
+        return point;
+    }
+
+    /*--------------------------------------------------------*/
+    /* Nested classes                                         */
+    /*--------------------------------------------------------*/
+
+    private static final class Point {
+        private int row;
+        private int col;
     }
 }
